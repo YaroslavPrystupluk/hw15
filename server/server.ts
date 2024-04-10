aa139a7ce474dbcbf5782675f2c388322224c349
Yaroslav Prystupliuk <tyucha_05@ukr.net>
2024-04-10 08:05:37 +0300

    add

--- server/server.ts
+++ server/server.ts
@@ -80,7 +80,7 @@ class App {
     this.app.use(express.static(path.join(this.publicPath)))
    //  this.app.get('/', this.mainController.getStartPage)
 	this.app.get('/', (req: Request, res: Response) => {
-		res.json({ message: 'Hello World!' })
+		res.send('Hello World!')
 	})
 
     const createUpdateMiddlewares = [
