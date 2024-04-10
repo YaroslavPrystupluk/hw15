9baf715eb4222232be03e3641b3d0b5b9e8ab53d
Yaroslav Prystupliuk <tyucha_05@ukr.net>
2024-04-10 16:51:40 +0300

    asss

--- server/server.ts
+++ server/server.ts
@@ -29,7 +29,7 @@ const validatorService = new Validator()
 const PORT: number | string = process.env.PORT || 3000
 const HOST: string = process.env.HOST || 'localhost'
 const CORS_OPTIONS = {
-  origin: 'http://',
+  origin: 'https://hw15-newsposts.vercel.app',
   optionsSuccessStatus: 200
 }
 
