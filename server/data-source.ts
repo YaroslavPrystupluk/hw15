fd1fe7fb7d35a0751f33731f48187c0da93a4567
Yaroslav Prystupliuk <tyucha_05@ukr.net>
2024-04-11 11:04:35 +0300

    add

--- server/data-source.ts
+++ server/data-source.ts
@@ -14,8 +14,8 @@ console.log(path)
 
 const options: DataSourceOptions & SeederOptions = {
   type: 'postgres',
+  url: process.env.DATABASE_URL,
   host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
--- server/repository/DatabaseConnection.ts
+++ server/repository/DatabaseConnection.ts
@@ -16,8 +16,8 @@ class DatabaseConnection {
   private constructor () {
     this.options = {
       type: 'postgres',
+		url: process.env.DATABASE_URL,
       host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
