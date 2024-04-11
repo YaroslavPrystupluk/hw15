8eb0e729e06a68b50710747e5f7dde60294da35f
Yaroslav Prystupliuk <tyucha_05@ukr.net>
2024-04-11 11:06:48 +0300

    asdf

--- server/build/errors/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NewspostsServiceError = exports.ValidationError = void 0;
-var ValidationError_1 = require("./ValidationError");
-Object.defineProperty(exports, "ValidationError", { enumerable: true, get: function () { return ValidationError_1.ValidationError; } });
-var NewspostsServiceError_1 = require("./NewspostsServiceError");
-Object.defineProperty(exports, "NewspostsServiceError", { enumerable: true, get: function () { return NewspostsServiceError_1.NewspostsServiceError; } });
-//# sourceMappingURL=index.js.map
